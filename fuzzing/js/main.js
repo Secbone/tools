c7029aeed41f1e1256a0ce3e4397987b180e450d
Secbone <secbone@gmail.com>
2014-12-23 22:10:13 +0800

    一曲高歌千行泪，情在回肠荡气中

--- fuzzing/js/main.js
+++ fuzzing/js/main.js
@@ -1,6 +1,27 @@
-(function(){
+(function() {
     console.log("start");
     var exp = '<a href="http://:{char}aidu.com">:{i}</a>';
-    var callback = 'for(var i in document.links)'
+    var callback = 'for(var i in document.links){if(document.links[i].hostname=="baidu.com"){push(i)}}';
+    var start = 0;
+    var end = 65535;
+
+    var result = [];
+
+    var $expEl = document.getElementById("exp");
+    var $cackEl = document.getElementById("cack");
+    var $sizeEl = document.getElementById("size");
+
+    function push(data) {
+        result.push(data);
+    }
+
+
+    function startFuzzy() {
+        for(var counter = start; counter <= end; counter++) {
+
+        }
+    }
+
+
 
 })();
